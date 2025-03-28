a63d8a28f8de6f906cff150f8a463558614683ea
Ri4yy <maksimov.1913@list.ru>
2025-03-28 17:32:45 +0300

    add site

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,22 @@
 document.addEventListener('DOMContentLoaded', () => {
-    let menu = document.querySelector('.menu'),
-    btnMenu = document.querySelector('.btn-menu');
+    let menu = document.querySelector('.header-mobile'),
+    btnMenu = document.querySelector('.btn-menu'),
+    html = document.querySelector('html');
 
     btnMenu.addEventListener('click', (e) => {
-        menu.classList.toggle('menu--open')
+        menu.classList.toggle('open')
 
         btnMenu.classList.toggle('btn-menu--open')
+        html.classList.toggle('no-scroll')
     })
 
     function resize() {
         let width = window.innerWidth;
 
-        if (width > 768) {
-            menu.classList.remove('menu--open')
+        if (width > 1280) {
+            menu.classList.remove('open')
             btnMenu.classList.remove('btn-menu--open')
+            html.classList.remove('no-scroll')
         } else {
             return
         }
@@ -23,4 +26,55 @@ document.addEventListener('DOMContentLoaded', () => {
         resize()
     })
     resize()
+
+    let menuLink = document.querySelectorAll('.header-mobile__link')
+
+    menuLink.forEach(link => {
+        link.addEventListener('click', (e) => {
+            menu.classList.remove('open')
+            btnMenu.classList.remove('btn-menu--open')
+            html.classList.remove('no-scroll')
+        })
+    })
+
+    let faqElements = document.querySelectorAll('.faq__item')
+
+    faqElements.forEach(element => {
+        let toggle = element.querySelector('.faq__item-question')
+
+        toggle.addEventListener('click', () => {
+            element.classList.toggle('active')
+        })
+    })
+
+    // Модальное окно
+    function showModal(btnOpen, modalBody) {
+        btnOpen.click(function () {
+            modalBody.addClass('active');
+            $('html').addClass('no-scroll');
+            return false;
+        });
+
+        $(document).keydown(function (e) {
+            if (e.keyCode === 27) {
+                e.stopPropagation();
+                modalBody.removeClass('active');
+                $('html').removeClass('no-scroll');
+            }
+        });
+
+        modalBody.click(function (e) {
+            if ($(e.target).closest('.modal__wrapper').length == 0) {
+                $(this).removeClass('active');
+                $('html').removeClass('no-scroll');
+            }
+        });
+
+        $('.close-modal').click((e) => {
+            modalBody.removeClass('active');
+            $('html').removeClass('no-scroll');
+        })
+    }
+
+    showModal($('.open-modal'), $('.modal--form'));
 })
--- assets/js/swiper.js
+++ assets/js/swiper.js
@@ -1,72 +1,92 @@
-const swiper = new Swiper('.swiper', {
-    // Optional parameters
-    spaceBetween: 20,
-    enabled: true,
+// const swiper = new Swiper('.swiper', {
+//     // Optional parameters
+//     spaceBetween: 20,
+//     enabled: true,
 
-    breakpoints: {
-        320: {
-            allowTouchMove: true,
-            slidesPerView: 1,
-            grid: {
-                rows: 1,
-                fill: "row",
-            }
-        },
-        480: {
-            allowTouchMove: true,
-            slidesPerView: 2,
-            grid: {
-                rows: 1,
-                fill: "row",
-            }
-        },
-        768: {
-            allowTouchMove: true,
-            slidesPerView: 3,
-            grid: {
-                rows: 1,
-                fill: "row",
-            }
-        },
-        1024: {
-            allowTouchMove: true,
-            slidesPerView: 2,
-            grid: {
-                rows: 2,
-                fill: "row",
-            }
-        },
-        1280: {
-            allowTouchMove: true,
-            slidesPerView: 3,
-            grid: {
-                rows: 2,
-                fill: "row",
-            }
-        },
-        1921: {
-            allowTouchMove: true,
-            slidesPerView: 4,
-            grid: {
-                rows: 2,
-                fill: "row",
-            }
-        },
+//     breakpoints: {
+//         320: {
+//             allowTouchMove: true,
+//             slidesPerView: 1,
+//             grid: {
+//                 rows: 1,
+//                 fill: "row",
+//             }
+//         },
+//         480: {
+//             allowTouchMove: true,
+//             slidesPerView: 2,
+//             grid: {
+//                 rows: 1,
+//                 fill: "row",
+//             }
+//         },
+//         768: {
+//             allowTouchMove: true,
+//             slidesPerView: 3,
+//             grid: {
+//                 rows: 1,
+//                 fill: "row",
+//             }
+//         },
+//         1024: {
+//             allowTouchMove: true,
+//             slidesPerView: 2,
+//             grid: {
+//                 rows: 2,
+//                 fill: "row",
+//             }
+//         },
+//         1280: {
+//             allowTouchMove: true,
+//             slidesPerView: 3,
+//             grid: {
+//                 rows: 2,
+//                 fill: "row",
+//             }
+//         },
+//         1921: {
+//             allowTouchMove: true,
+//             slidesPerView: 4,
+//             grid: {
+//                 rows: 2,
+//                 fill: "row",
+//             }
+//         },
+//     },
+  
+//     // If we need pagination
+//     pagination: {
+//       el: '.swiper-pagination',
+//     },
+  
+//     // Navigation arrows
+//     navigation: {
+//       nextEl: '.swiper-button-next',
+//       prevEl: '.swiper-button-prev',
+//     },
+  
+//     // And if we need scrollbar
+//     scrollbar: {
+//       el: '.swiper-scrollbar',
+//     },
+// });
+
+const swiperAbout = new Swiper('.swiper-about', {
+    // Optional parameters
+    enabled: true,
+    effect: "fade",
+    fadeEffect: {
+        crossFade: true
     },
   
     // If we need pagination
     pagination: {
-      el: '.swiper-pagination',
+      el: '.swiper-about__pagination',
     },
   
     // Navigation arrows
     navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-  
-    // And if we need scrollbar
-    scrollbar: {
-      el: '.swiper-scrollbar',
+      nextEl: '.swiper-about__btn--next',
+      prevEl: '.swiper-about__btn--prev',
     },
 });
